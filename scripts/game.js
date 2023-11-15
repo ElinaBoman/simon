e2c5fcb296a28d150ee866f33781fe515f3c369c
ElinaBoman Boman <elinaholmberg@hotmail.com>
2023-11-15 14:10:22 +0000

    addTurn function

--- scripts/game.js
+++ scripts/game.js
@@ -3,11 +3,29 @@ let game = {
     score: 0, 
     currentGame: [],
     playerMoves: [],
-    choices: []
+    choices: ["button1", "button2", "button3", "button4"]
 }
 
+function newGame () {
+   game.score = 0;
+   game.currentGame = [];
+   game.playerMoves = [];
+   showScore();
+   addTurn();
+    }
+
+function showScore () {
+    document.getElementById("score").innerText = game.score;
+};
+
+
+function addTurn () {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    // showTurns();
+}
 
 /**exports används i Node.js för att exportera en modul. 
 I detta fall exporteras ett objekt som har en egenskap med namnet game,
 och värdet av den egenskapen är det värde som är associerat med variabeln game i din kod.*/
-module.exports = { game };
+module.exports = { game, newGame, showScore, addTurn };
--- tests/game.test.js
+++ tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game } = require("../scripts/game");
+const { game, newGame, showScore, addTurn } = require("../scripts/game");
 
 beforeAll(() => {
     let fs = require("fs");
@@ -24,5 +24,30 @@ test("Score key exists", () => {
     });
     test("choices key exists", () => {
     expect("choices" in game).toBe(true);
+    });
+    test("choices contain correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+        });
+});
+
+describe("new game works correctly", () => {
+beforeAll(() => {
+game.score = 42;
+game.playerMoves = ["button1", "button2"];
+game.currentGame = ["button1", "button2"];
+document.getElementById("score").innerText = "42";
+newGame();
+});
+test("should set playerMoves to 0", () => {
+expect(game.score).toEqual(0);
+});
+test("should set gamescore to 0", () => {
+expect(game.playerMoves.length).toEqual(0);
+});
+test("should be on element in computer array", () => {
+    expect(game.currentGame.length).toBe(1);
+});
+test("should display 0 for the element with id score", () => {
+    expect(document.getElementById("score").innerText).toEqual(0);
     });
 });
